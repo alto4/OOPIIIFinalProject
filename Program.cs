2b01f956368c3f3548c67fc9603bcd9af597809c
Scott Alton <scottaltonmusic@gmail.com>
2021-02-05 14:19:22 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOPFinalProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
