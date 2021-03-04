dad953a3e99292fb62c5178726a314e082764e9a
Scott Alton <scottaltonmusic@gmail.com>
2021-03-04 17:19:36 -0500

    Added article reference for enums and tidied up formatting.

--- Rank.cs
+++ Rank.cs
@@ -12,21 +12,23 @@ namespace OOPFinalProject
 {
     /// <summary>
     /// Enum for rank of a card.
+    /// REFERENCE SOURCE: https://www.tutorialsteacher.com/csharp/csharp-enum
+    /// REFERENCE DESCRIPTION: I found this article as a solid source for explaining how enums work and how to assign specific values to each member
     /// </summary>
     public enum Rank
     {
-        Ace=1,
-        King=13,
-        Queen=12,
-        Jack=11,
-        Ten=10,
-        Nine=9,
-        Eight=8,
-        Seven=7,
-        Six=6,
-        Five=5,
-        Four=4,
-        Three=3,
-        Two=2
+        Ace = 1,
+        King = 13,
+        Queen = 12,
+        Jack = 11,
+        Ten = 10,
+        Nine = 9,
+        Eight = 8,
+        Seven = 7,
+        Six = 6,
+        Five = 5,
+        Four = 4,
+        Three = 3,
+        Two = 2
     }
 }
