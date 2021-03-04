864f13e3b1940f18857eba3fce2602c1529e8a29
Scott Alton <scottaltonmusic@gmail.com>
2021-03-03 20:03:36 -0500

    Roughed in new comparison method for computer to determine best choice in current hand. Need to extract from Program.cs

--- Rank.cs
+++ Rank.cs
@@ -11,18 +11,18 @@ namespace OOPFinalProject
     /// </summary>
     public enum Rank
     {
-        Ace,
-        King,
-        Queen,
-        Jack,
-        Ten,
-        Nine,
-        Eight,
-        Seven,
-        Six,
-        Five,
-        Four,
-        Three,
-        Two
+        Ace=1,
+        King=13,
+        Queen=12,
+        Jack=11,
+        Ten=10,
+        Nine=9,
+        Eight=8,
+        Seven=7,
+        Six=6,
+        Five=5,
+        Four=4,
+        Three=3,
+        Two=2
     }
 }
